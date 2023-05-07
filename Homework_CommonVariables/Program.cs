380b090c97630aa8cab2e41c1803bc2bceeb3e1b
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 17:30:01 -0600

    Add project files.

---
+++ Homework_CommonVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Create a Console Application that has variables to hold a person's
+// name, age, if they are alive, and their phone number.
+// You do not need to capture these values from the user.
+
+//initialize variables
+string firstName = null;
+string lastName = null;
+int? age = null;
+bool? isAlive = null;
+string phoneNumber = null;
+
+//capture or assign values
+firstName = "Mohammad";
+lastName = "Jabir";
+age = 28;
+isAlive = true;
+phoneNumber = "(780)xxx-xxxx";
+
+//display result
+Console.WriteLine($"Hello {firstName} {lastName}. You are {age} years old. " +
+    $"Are you alive? T/F {isAlive}. Your phone number is {phoneNumber}.");
